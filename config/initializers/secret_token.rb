ea863b426d2582242e9a16901454e438bb3d19c7
Aditya Raisinghani <aditya.ajeet@gmail.com>
2013-09-05 17:54:35 +0530

    Add a full_title helper

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,19 +4,7 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-require 'securerandom'
 
-def secure_token
-  token_file = Rails.root.join('.secret')
-  if File.exist?(token_file)
-    # Use the existing token.
-    File.read(token_file).chomp
-  else
-    # Generate a new token and store it in token_file.
-    token = SecureRandom.hex(64)
-    File.write(token_file, token)
-    token
-  end
-end
 
-SampleApp::Application.config.secret_key_base = secure_token
+
+SampleApp::Application.config.secret_token = '8657b733ffe513d128619a233c71f452169c05e5652fcebe82d48c9301ce65a79f95b27a9ca4dfe5a2dc546641d3457362ccd0c4651767f453bf331b9aeca1dc'
